6eb29eeff90a2fa4c67074683312e680ec4d8108
nikkiBubencik <114317457+nikkiBubencik@users.noreply.github.com>
2025-05-20 15:13:43 -0400

    Update Home.tsx
    update description

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -7,16 +7,15 @@ const Home: React.FC = () => {
             <h1>Nicole Bubencik</h1>
             <div>
                 <h2>About Me</h2>
-                <p>Hi, I am Nicole Bubencik, a graduate student in Computer Information Systems with a concentration in Web Application Development at Boston University. I graduated with a Bachelor's degree in Computer Science and Mathematics in May. I am passionate about building innovative web applications and solving complex problems through technology. My academic background has given me a strong foundation in both theoretical concepts and hands-on development, and I am eager to apply this knowledge in a professional setting.</p>
-                <p>Through my academic and professional experiences, I have gained knowledge in technologies such as React, JavaScript, Python, and more. In my IT Tech role, I taught myself React to automate an internal visitor management system that was previously manual. The system now allows visitors to sign in, automatically determine if they need to sign a new or updated Confidentiality Disclosure Agreement (CDA), and prints a badge based on their citizenship status—blue for U.S. citizens and yellow for others. This project not only improved operational efficiency but also reinforced my skills in web development, problem-solving, and full-stack integration.</p>
-                <p>I am excited to continue developing my skills and contribute to impactful projects in the web development field.</p>
-                <p><Link to="/resume" className="page-link">Resume</Link></p>
+                <p>Hi, I'm Nicole Bubencik—a graduate student in Computer Information Systems at Boston University, concentrating in Web Application Development. I hold dual Bachelor's degrees in Computer Science and Mathematics from the University of Rhode Island. I'm passionate about building innovative web applications and solving complex problems through technology. My academic background has equipped me with a solid foundation in both theoretical concepts and hands-on development, and I’m eager to apply this knowledge in a professional setting.</p>
+                <p>Through my academic and professional experiences, I’ve gained practical knowledge of technologies like React, TypeScript, and Angular. While working as an IT Technician, I self-taught React to automate an internal visitor management system that had previously been manual. The new system allows visitors to sign in, checks whether they need to sign or update a Confidentiality Disclosure Agreement (CDA), and prints a badge based on their citizenship status—blue for U.S. citizens and yellow for others. This project not only improved operational efficiency but also reinforced my skills in web development, full-stack integration, and creative problem-solving.</p>
+                <p>I’m excited to continue growing as a developer and contribute to impactful, user-focused projects in the software development field.</p>
                 <p><Link to="/projects" className="page-link">Projects</Link></p>
             </div>
-            <div className="home-page-card">
+{/*             <div className="home-page-card">
                 <h2>My Interests</h2>
                 <p>Outside of coding, I enjoy running, reading, hiking, and taking care of my plants. I also enjoy traveling; my family and I recently hiked in Zion National Park in Utah and have visited many states. Outside the United States, I have visited countries including England, Ireland, France, Spain, Portugal, and Costa Rica.</p>
-            </div>
+            </div> */}
         </div>
     )
 }
